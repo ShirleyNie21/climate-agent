74c8cbcb613f0a4c76aa76073286df63ad7231b9
ShirleyNie21 <shirleywnie@gmail.com>
2026-04-25 15:31:54 -0400

    Fetch current weather data from Open Meteo API

--- climate_ai_agent.py
+++ climate_ai_agent.py
@@ -1,1 +1,9 @@
+import requests
 
+url = "https://api.open-meteo.com/v1/forecast?latitude=40.7&longitude=-74.0&current_weather=true"
+
+response = requests.get(url)
+
+data = response.json()
+
+print(data)
